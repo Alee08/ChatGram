937a976dbdbcb624520eb4a30bb97e054e03e898
Alessandro Trapasso <39519734+Alee08@users.noreply.github.com>
2018-06-07 18:26:11 +0200

    Update wmbus_amb8465.cc

--- wmbus_amb8465.cc
+++ wmbus_amb8465.cc
@@ -1,3 +1,24 @@
+/*
+Copyright (C) 2017 Fredrik Öhrström
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+Additional code in this file:
+Copyright (c) 2018 Alessandro Trapasso
+Also licensed to you under GPLv3 as above.
+*/
 #include"wmbus.h"
 #include"wmbus_amb8465.h"
 #include"serial.h"
